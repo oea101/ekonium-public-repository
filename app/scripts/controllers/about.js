d5916aee963c4d384fd7483f572b35b3f24d7b06
Emmanuel <oea101@gmail.com>
2015-11-04 13:06:34 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ekoniumPublicRepositoryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ekoniumPublicRepositoryApp
+ */
+angular.module('ekoniumPublicRepositoryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ekoniumPublicRepositoryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ekoniumPublicRepositoryApp
+ */
+angular.module('ekoniumPublicRepositoryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
